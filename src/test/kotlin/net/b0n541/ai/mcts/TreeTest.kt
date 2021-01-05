7e90d86a7e07b2a08535f8b6b04601afb7f72e7a
b0n541 <j@nschaefer.net>
2021-01-05 15:57:06 +0100

    Remove warning

--- src/test/kotlin/net/b0n541/ai/mcts/TreeTest.kt
+++ src/test/kotlin/net/b0n541/ai/mcts/TreeTest.kt
@@ -2,12 +2,11 @@ package net.b0n541.ai.mcts
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import java.util.List
 
 internal class TreeTest {
     @Test
     fun emptyTree() {
-        val tree = Tree(TestGameState(List.of("HEADS", "TAILS")))
+        val tree = Tree(TestGameState(listOf("HEADS", "TAILS")))
         Assertions.assertThat(tree.size).isEqualTo(1)
         tree.rootNode.expandPossibleMoves()
         Assertions.assertThat(tree.size).isEqualTo(3)
