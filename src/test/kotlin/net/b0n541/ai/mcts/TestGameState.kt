0037137dc45154d6e3923cc64c9f4efe0c5d2386
b0n541 <j@nschaefer.net>
2021-01-08 14:51:22 +0100

    Remove leftovers from migration from Java

--- src/test/kotlin/net/b0n541/ai/mcts/TestGameState.kt
+++ src/test/kotlin/net/b0n541/ai/mcts/TestGameState.kt
@@ -1,10 +1,7 @@
 package net.b0n541.ai.mcts
 
-import java.util.*
-
-internal class TestGameState(players: List<String>) : GameState<GameMove> {
+internal class TestGameState(override val players: List<String>) : GameState<GameMove> {
     private val moves: MutableList<GameMove> = mutableListOf()
-    override val players: List<String>
 
     private constructor(players: List<String>, moves: List<GameMove>, move: GameMove) : this(players) {
         this.moves.addAll(moves)
@@ -12,7 +9,7 @@ internal class TestGameState(players: List<String>) : GameState<GameMove> {
     }
 
     override val possibleMoves: List<GameMove>
-        get() = java.util.List.of(TestGameMove.HEADS, TestGameMove.TAILS)
+        get() = listOf(TestGameMove.HEADS, TestGameMove.TAILS)
 
     override fun addMove(move: GameMove): GameState<GameMove> {
         return TestGameState(players, moves, move)
@@ -20,25 +17,23 @@ internal class TestGameState(players: List<String>) : GameState<GameMove> {
 
     override val isGameFinished: Boolean
         get() = moves.size == 100
+
     override val gameValues: Map<String, Double>
         get() {
             val headsGameValue = 1.0 *
                     moves.stream()
                         .filter { move: GameMove -> move === TestGameMove.HEADS }
                         .count()
-            return java.util.Map.of(
-                TestGameMove.HEADS.toString(),
-                headsGameValue,
-                TestGameMove.TAILS.toString(),
-                100 - headsGameValue
+
+            return mapOf(
+                TestGameMove.HEADS.toString() to headsGameValue,
+                TestGameMove.TAILS.toString() to 100 - headsGameValue
             )
         }
+
     override val lastMove: GameMove
         get() = moves[moves.size - 1]
+
     override val nextPlayer: String
         get() = players[0]
-
-    init {
-        this.players = Collections.unmodifiableList(players)
-    }
 }
