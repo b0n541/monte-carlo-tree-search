7335c042f2506f65682c304b6bd8dd6e81655fe2
b0n541 <j@nschaefer.net>
2021-01-20 22:35:41 +0100

    Feed board state into computation graph

--- src/main/kotlin/net/b0n541/ai/game/connect4/Connect4.kt
+++ src/main/kotlin/net/b0n541/ai/game/connect4/Connect4.kt
@@ -1,5 +1,6 @@
 package net.b0n541.ai.game.connect4
 
+import net.b0n541.ai.game.connect4.player.alphazero.AlphaZeroConnect4Player
 import net.b0n541.ai.game.connect4.player.simple.MctsConnect4Player
 import org.slf4j.LoggerFactory
 
@@ -10,7 +11,7 @@ object Connect4 {
     @JvmStatic
     fun playGame() {
         var gameState = Connect4GameState(Connect4PlayerSymbol.O)
-        val noughtsPlayer = MctsConnect4Player(Connect4PlayerSymbol.O, Connect4PlayerSymbol.O)
+        val noughtsPlayer = AlphaZeroConnect4Player(Connect4PlayerSymbol.O, Connect4PlayerSymbol.O)
         val crossesPlayer = MctsConnect4Player(Connect4PlayerSymbol.X, Connect4PlayerSymbol.O)
 
         LOG.info(gameState.toString())
--- src/main/kotlin/net/b0n541/ai/game/connect4/player/alphazero/AlphaZeroConnect4Player.kt
+++ src/main/kotlin/net/b0n541/ai/game/connect4/player/alphazero/AlphaZeroConnect4Player.kt
@@ -3,11 +3,70 @@ package net.b0n541.ai.game.connect4.player.alphazero
 import net.b0n541.ai.game.connect4.Connect4Move
 import net.b0n541.ai.game.connect4.Connect4PlayerSymbol
 import net.b0n541.ai.game.connect4.player.AbstractConnect4Player
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.deeplearning4j.nn.conf.layers.DenseLayer
+import org.deeplearning4j.nn.conf.layers.OutputLayer
+import org.deeplearning4j.nn.graph.ComputationGraph
+import org.nd4j.linalg.lossfunctions.LossFunctions
+import org.slf4j.LoggerFactory
 
 class AlphaZeroConnect4Player(playerSymbol: Connect4PlayerSymbol, firstPlayer: Connect4PlayerSymbol) :
     AbstractConnect4Player(playerSymbol, firstPlayer) {
 
+    private val network: ComputationGraph = initGraph()
+
+    private fun initGraph(): ComputationGraph {
+        val config = NeuralNetConfiguration.Builder()
+            .graphBuilder()
+            .addInputs("input")
+            .addLayer(
+                "L1",
+                DenseLayer.Builder().nIn(43).nOut(1024).build(),
+                "input"
+            )
+            .addLayer(
+                "out1",
+                OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).nIn(1024).nOut(2)
+                    .build(),
+                "L1"
+            )
+            .addLayer(
+                "out2",
+                OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).nIn(1024).nOut(7).build(),
+                "L1"
+            )
+            .setOutputs("out1", "out2")
+            .build()
+
+        val graph = ComputationGraph(config)
+        graph.init()
+
+        return graph
+    }
+
     override fun play(): Connect4Move {
-        return gameState.possibleMoves.random()
+
+        val input = gameState.networkInputs
+        val outputs = network.output(input)
+
+        LOG.info("Game value prediction: ${outputs[0]}")
+
+        var bestMove = -1
+        var highestMoveValue = Double.MIN_VALUE
+        LOG.info("Move prediction:")
+        for (i in 0..6) {
+            var moveValue = outputs[1].getDouble(i)
+            LOG.info("Move $i value: $moveValue.")
+            if (moveValue > highestMoveValue) {
+                highestMoveValue = moveValue
+                bestMove = i
+            }
+        }
+        LOG.info("Best move: $bestMove")
+        return Connect4Move(playerSymbol, bestMove)
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(AlphaZeroConnect4Player::class.java)
     }
 }
