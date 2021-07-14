955797e0900004e7f8a0da88c97d6720cb7aa12b
b0n541 <j@nschaefer.net>
2021-07-14 23:28:38 +0200

    Add more test cases and explicit implementation

--- src/main/kotlin/net/b0n541/ai/tournament/Tournament.kt
+++ src/main/kotlin/net/b0n541/ai/tournament/Tournament.kt
@@ -1,23 +1,150 @@
 package net.b0n541.ai.tournament
 
-import net.b0n541.ai.game.tictactoe.player.TicTacToePlayer
+import net.b0n541.ai.game.common.GameMove
+import net.b0n541.ai.game.common.GamePlayer
+import net.b0n541.ai.game.common.PlayerSymbol
+import kotlin.math.pow
 
-fun getPlayerCombinations(
+fun <S : PlayerSymbol, M : GameMove> getPlayerCombinations(
     numberOfPlayerPerGame: Int,
-    player: List<TicTacToePlayer>
-): List<List<TicTacToePlayer>> {
-    val result = mutableListOf<MutableList<TicTacToePlayer>>()
+    player: List<GamePlayer<S, M>>
+): List<List<GamePlayer<S, M>>> {
+    val result = mutableListOf<MutableList<GamePlayer<S, M>>>()
 
-    repeat((player.size * numberOfPlayerPerGame)) { result.add(mutableListOf()) }
+    repeat(numberOfGames(numberOfPlayerPerGame, player.size)) {
+        result.add(mutableListOf())
+    }
 
-    result[0].add(player[0])
-    result[0].add(player[0])
-    result[1].add(player[0])
-    result[1].add(player[1])
-    result[2].add(player[1])
-    result[2].add(player[0])
-    result[3].add(player[1])
-    result[3].add(player[1])
+    // TODO Generalize with tree
+    if (numberOfPlayerPerGame == 2) {
+        if (player.size == 2) {
+            result[0].add(player[0])
+            result[1].add(player[1])
+
+            result[0].add(player[1])
+            result[1].add(player[0])
+        }
+        if (player.size == 3) {
+            result[0].add(player[0])
+            result[1].add(player[0])
+            result[2].add(player[1])
+            result[3].add(player[1])
+            result[4].add(player[2])
+            result[5].add(player[2])
+
+            result[0].add(player[1])
+            result[1].add(player[2])
+            result[2].add(player[0])
+            result[3].add(player[2])
+            result[4].add(player[0])
+            result[5].add(player[1])
+        }
+    }
+    if (numberOfPlayerPerGame == 3) {
+        if (player.size == 2) {
+            result[0].add(player[0])
+            result[1].add(player[0])
+            result[2].add(player[0])
+            result[3].add(player[1])
+            result[4].add(player[1])
+            result[5].add(player[1])
+
+            result[0].add(player[0])
+            result[1].add(player[1])
+            result[2].add(player[1])
+            result[3].add(player[1])
+            result[4].add(player[0])
+            result[5].add(player[0])
+
+            result[0].add(player[1])
+            result[1].add(player[0])
+            result[2].add(player[1])
+            result[3].add(player[0])
+            result[4].add(player[1])
+            result[5].add(player[0])
+        }
+        if (player.size == 3) {
+            result[0].add(player[0])
+            result[1].add(player[0])
+            result[2].add(player[0])
+            result[3].add(player[0])
+            result[4].add(player[0])
+            result[5].add(player[0])
+            result[6].add(player[0])
+            result[7].add(player[0])
+            result[8].add(player[1])
+            result[9].add(player[1])
+            result[10].add(player[1])
+            result[11].add(player[1])
+            result[12].add(player[1])
+            result[13].add(player[1])
+            result[14].add(player[1])
+            result[15].add(player[1])
+            result[16].add(player[2])
+            result[17].add(player[2])
+            result[18].add(player[2])
+            result[19].add(player[2])
+            result[20].add(player[2])
+            result[21].add(player[2])
+            result[22].add(player[2])
+            result[23].add(player[2])
+
+            result[0].add(player[0])
+            result[1].add(player[0])
+            result[2].add(player[1])
+            result[3].add(player[1])
+            result[4].add(player[2])
+            result[5].add(player[2])
+            result[6].add(player[1])
+            result[7].add(player[2])
+            result[8].add(player[1])
+            result[9].add(player[1])
+            result[10].add(player[0])
+            result[11].add(player[0])
+            result[12].add(player[2])
+            result[13].add(player[2])
+            result[14].add(player[0])
+            result[15].add(player[2])
+            result[16].add(player[2])
+            result[17].add(player[2])
+            result[18].add(player[0])
+            result[19].add(player[0])
+            result[20].add(player[1])
+            result[21].add(player[1])
+            result[22].add(player[0])
+            result[23].add(player[1])
+
+            result[0].add(player[1])
+            result[1].add(player[2])
+            result[2].add(player[0])
+            result[3].add(player[1])
+            result[4].add(player[0])
+            result[5].add(player[2])
+            result[6].add(player[2])
+            result[7].add(player[1])
+            result[8].add(player[0])
+            result[9].add(player[2])
+            result[10].add(player[1])
+            result[11].add(player[0])
+            result[12].add(player[1])
+            result[13].add(player[2])
+            result[14].add(player[2])
+            result[15].add(player[0])
+            result[16].add(player[0])
+            result[17].add(player[1])
+            result[18].add(player[2])
+            result[19].add(player[0])
+            result[20].add(player[2])
+            result[21].add(player[1])
+            result[22].add(player[1])
+            result[23].add(player[0])
+        }
+    }
 
     return result.toList()
 }
+
+private fun numberOfGames(
+    numberOfPlayerPerGame: Int,
+    numberOfPlayerTypes: Int
+) = (numberOfPlayerTypes.toDouble().pow(numberOfPlayerPerGame.toDouble()) - numberOfPlayerTypes).toInt()
--- src/test/kotlin/net/b0n541/ai/tournament/TournamentTest.kt
+++ src/test/kotlin/net/b0n541/ai/tournament/TournamentTest.kt
@@ -1,5 +1,8 @@
 package net.b0n541.ai.tournament
 
+import net.b0n541.ai.game.connect4.player.HumanConnect4Player
+import net.b0n541.ai.game.connect4.player.MctsConnect4Player
+import net.b0n541.ai.game.connect4.player.RandomConnect4Player
 import net.b0n541.ai.game.tictactoe.player.MctsTicTacToePlayer
 import net.b0n541.ai.game.tictactoe.player.RandomTicTacToePlayer
 import org.assertj.core.api.Assertions.assertThat
@@ -14,10 +17,79 @@ internal class TournamentTest {
         val result = getPlayerCombinations(2, listOf(first, second))
 
         assertThat(result).containsExactlyInAnyOrder(
-            listOf(first, first),
             listOf(first, second),
+            listOf(second, first)
+        )
+    }
+
+    @Test
+    fun `3 player types in 2 player game`() {
+        val first = RandomConnect4Player()
+        val second = MctsConnect4Player()
+        val third = HumanConnect4Player()
+
+        val result = getPlayerCombinations(2, listOf(first, second, third))
+
+        assertThat(result).containsExactlyInAnyOrder(
+            listOf(first, second),
+            listOf(first, third),
             listOf(second, first),
-            listOf(second, second)
+            listOf(second, third),
+            listOf(third, first),
+            listOf(third, second)
+        )
+    }
+
+    @Test
+    fun `2 player types in 3 player game`() {
+        val first = RandomConnect4Player()
+        val second = MctsConnect4Player()
+
+        val result = getPlayerCombinations(3, listOf(first, second))
+
+        assertThat(result).containsExactlyInAnyOrder(
+            listOf(first, first, second),
+            listOf(first, second, first),
+            listOf(first, second, second),
+            listOf(second, second, first),
+            listOf(second, first, second),
+            listOf(second, first, first)
+        )
+    }
+
+    @Test
+    fun `3 player types in 3 player game`() {
+        val first = RandomConnect4Player()
+        val second = MctsConnect4Player()
+        val third = HumanConnect4Player()
+
+        val result = getPlayerCombinations(3, listOf(first, second, third))
+
+        assertThat(result).containsExactlyInAnyOrder(
+            listOf(first, first, second),
+            listOf(first, first, third),
+            listOf(first, second, first),
+            listOf(first, second, second),
+            listOf(first, third, first),
+            listOf(first, third, third),
+            listOf(first, second, third),
+            listOf(first, third, second),
+            listOf(second, second, first),
+            listOf(second, second, third),
+            listOf(second, first, second),
+            listOf(second, first, first),
+            listOf(second, third, second),
+            listOf(second, third, third),
+            listOf(second, first, third),
+            listOf(second, third, first),
+            listOf(third, third, first),
+            listOf(third, third, second),
+            listOf(third, first, third),
+            listOf(third, first, first),
+            listOf(third, second, third),
+            listOf(third, second, second),
+            listOf(third, first, second),
+            listOf(third, second, first)
         )
     }
 }
